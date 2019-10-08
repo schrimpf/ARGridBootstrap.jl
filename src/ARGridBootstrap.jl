5123686d60801b3cf57b8b1aceadf68a2016f2ce
Paul Schrimpf <paul.schrimpf@gmail.com>
2019-10-08 16:51:31 -0700

    Files generated by PkgTemplates

---
+++ src/ARGridBootstrap.jl
@@ -0,0 +1,5 @@
+module ARGridBootstrap
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ARGridBootstrap
+using Test
+
+@testset "ARGridBootstrap.jl" begin
+    # Write your own tests here.
+end
